43996bcc7e2704fe275b80109ee1b31ae81a1784
Aditya Banerjee <86280593+Aditya12705@users.noreply.github.com>
2025-06-04 01:03:40 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  API_URL: process.env.REACT_APP_API_URL || 'https://alethemyra-backend.onrender.com',
   // Add other configuration variables as needed
 };
 
-export default config; 
+export default config;
